058905239bfdfed9817017b003596e988b207e6a
Sean O'Hare <seanohare@link.cuhk.edu.hk>
2019-12-13 20:22:03 +0800

    serve CRA build

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 let express = require('express');
+const path = require('path');
 let app = express();
 const cors = require('cors');
 const resultsController = require('./results/resultsController');
 const athletesController = require('./athletes/athletesController');
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/results', resultsController);
 app.use('/athletes', athletesController);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(function (req, res) {
     res.status(404).send("Sorry can't find that API route!")
---
+++ build/precache-manifest.2feee531e0ed28a4ea95037d1bd51033.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062d82e261141686693f8ef615978be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c31a531f7543e3a0d5f",
+    "url": "/static/css/2.2da83443.chunk.css"
+  },
+  {
+    "revision": "a2c0562fe77e20ad2db0",
+    "url": "/static/css/main.bff84a66.chunk.css"
+  },
+  {
+    "revision": "6c31a531f7543e3a0d5f",
+    "url": "/static/js/2.2d87e948.chunk.js"
+  },
+  {
+    "revision": "a2c0562fe77e20ad2db0",
+    "url": "/static/js/main.f2fdbe6a.chunk.js"
+  },
+  {
+    "revision": "036cf86247af2483cb4c",
+    "url": "/static/js/runtime~main.2c8370ee.js"
+  },
+  {
+    "revision": "17f4661402646db18537d346ad2e0f37",
+    "url": "/static/media/roboto-mono-latin-100.17f46614.woff2"
+  },
+  {
+    "revision": "cf6d683a8f9c5663563d12f466d42c59",
+    "url": "/static/media/roboto-mono-latin-100.cf6d683a.woff"
+  },
+  {
+    "revision": "3e35c3695d45b0e4c0ad5eb46e50415b",
+    "url": "/static/media/roboto-mono-latin-100italic.3e35c369.woff2"
+  },
+  {
+    "revision": "d553aa0cffa8231d3c4fa419c2113bbd",
+    "url": "/static/media/roboto-mono-latin-100italic.d553aa0c.woff"
+  },
+  {
+    "revision": "0c8fb655ddd6b7f87e08e410b30d1ecf",
+    "url": "/static/media/roboto-mono-latin-300.0c8fb655.woff2"
+  },
+  {
+    "revision": "44f888000468884edc2faf9e1c967294",
+    "url": "/static/media/roboto-mono-latin-300.44f88800.woff"
+  },
+  {
+    "revision": "e470999445ae32e747dee1b56813ffab",
+    "url": "/static/media/roboto-mono-latin-300italic.e4709994.woff2"
+  },
+  {
+    "revision": "f6e36c9799a9cf51247ad391dbabf923",
+    "url": "/static/media/roboto-mono-latin-300italic.f6e36c97.woff"
+  },
+  {
+    "revision": "0c94e034ca06357576c2d03d623e1fcd",
+    "url": "/static/media/roboto-mono-latin-400.0c94e034.woff2"
+  },
+  {
+    "revision": "6d5bc1d8c0dc6b8420463d00e33be71a",
+    "url": "/static/media/roboto-mono-latin-400.6d5bc1d8.woff"
+  },
+  {
+    "revision": "2820fa2acd542ff7b04d97821392d566",
+    "url": "/static/media/roboto-mono-latin-400italic.2820fa2a.woff2"
+  },
+  {
+    "revision": "fce1aae7a5427ca04e739cfc4d77a913",
+    "url": "/static/media/roboto-mono-latin-400italic.fce1aae7.woff"
+  },
+  {
+    "revision": "3f107f0a4cf59cba836c6cad6b76c224",
+    "url": "/static/media/roboto-mono-latin-500.3f107f0a.woff"
+  },
+  {
+    "revision": "6221bb5e9ca6f51a91ebcbe89ad10361",
+    "url": "/static/media/roboto-mono-latin-500.6221bb5e.woff2"
+  },
+  {
+    "revision": "8f683f310450928c8acc32caa82bb5b0",
+    "url": "/static/media/roboto-mono-latin-500italic.8f683f31.woff"
+  },
+  {
+    "revision": "b340d3a4eb064d6cbe74a55f51e3ea50",
+    "url": "/static/media/roboto-mono-latin-500italic.b340d3a4.woff2"
+  },
+  {
+    "revision": "73491684849da6c94511623a2f5ae93d",
+    "url": "/static/media/roboto-mono-latin-700.73491684.woff2"
+  },
+  {
+    "revision": "b5ee3da2876c44c6d7d2d6bac0cdba0b",
+    "url": "/static/media/roboto-mono-latin-700.b5ee3da2.woff"
+  },
+  {
+    "revision": "a516d634263cef2229a84c016cce883d",
+    "url": "/static/media/roboto-mono-latin-700italic.a516d634.woff2"
+  },
+  {
+    "revision": "b933305ebfd47a7f5d26380d97242113",
+    "url": "/static/media/roboto-mono-latin-700italic.b933305e.woff"
+  }
+]);
